12e0be36b76e860722e04093e6e8d581154634b0
Dan Rodriguez <dan@danrodriguez.me>
2013-02-03 18:32:12 -0500

    got first couple specs working

--- spec/lib/exception_spec.rb
+++ spec/lib/exception_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe Exception do
+  subject { Exception.new }
   describe "#to_google" do
-    it "should link to google"
+    it "should link to the google results" do
+      subject.to_google.should == "https://www.google.com/search?q=#{subject.message}"
+    end
+  end
+  describe "#to_stackoverflow" do
+    it "should link to the stackoverflow results" do
+      subject.to_stackoverflow.should == "http://stackoverflow.com/search?q=#{subject.message}"
+    end
   end
 end
