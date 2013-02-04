8f48dc00ce8096ff9e895996321a4a957b629e03
Dan Rodriguez <dan@danrodriguez.me>
2013-02-03 21:05:26 -0500

    added spec for link formatting

--- lib/exception_search/rspec.rb
+++ lib/exception_search/rspec.rb
@@ -8,13 +8,18 @@ module RSpec
         private
         
         def dump_backtrace(example)
-          exception = example.execution_result[:exception]
-
-          output.puts "#{long_padding} Search for this exception online: (#{instructions})"
-          output.puts "#{long_padding}#{short_padding} google: \t#{ exception.to_google }"
-          output.puts "#{long_padding}#{short_padding} stackoverflow: \t#{ exception.to_stackoverflow }"
+          output.puts format_links(example)
 
           dump_backtrace_original(example)
+        end
+
+        def format_links(example)
+          exception = example.execution_result[:exception]
+          "
+          #{long_padding} Search for this exception online: (#{instructions}) 
+          #{long_padding}#{short_padding} google: \t#{ exception.to_google }
+          #{long_padding}#{short_padding} stackoverflow: \t#{ exception.to_stackoverflow }
+          "
         end
 
         def instructions
--- spec/lib/rspec_spec.rb
+++ spec/lib/rspec_spec.rb
@@ -3,9 +3,23 @@ require 'spec_helper'
 describe RSpec::Core::Formatters::BaseTextFormatter do
 
   describe "#dump_backtrace" do
-    it "should show the links when a backtrace is printed" do
-      formatter = RSpec::Core::Formatters::BaseTextFormatter.new
-      formatter.dump_backtrace(nil).should include("http")
+    it "should call format_links"
+  end
+
+  describe "#format_links" do
+    it "should include the links when a backtrace is printed" do
+      formatter = RSpec::Core::Formatters::BaseTextFormatter.new(nil)
+
+      exception = Exception.new("message")
+      exception.set_backtrace([])
+
+      mock_example = mock(:example)
+      mock_example.stub(:execution_result).and_return({:exception => exception})
+      mock_example.stub(:metadata).and_return({})
+
+      result = formatter.instance_eval{ format_links(mock_example) }
+      result.should include("https://www.google.com/search?q=message")
+      result.should include("http://stackoverflow.com/search?q=message")
     end
   end
 end
